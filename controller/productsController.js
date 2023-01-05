8265957c9f278e7e430d60d7b62901433f00d071
Tiago Gomes <tidgomes@gmail.com>
2023-01-05 17:08:42 -0300

    get a single product

--- controller/productsController.js
+++ controller/productsController.js
@@ -3,6 +3,7 @@ const Product = require('../model/productModel')
 async function getProducts(req, res) {
   try {
     const products = await Product.findAll()
+
     res.writeHead(200, { 'Content-Type': 'application/json' })
     res.end(JSON.stringify(products))
   } catch (error) {
@@ -10,4 +11,15 @@ async function getProducts(req, res) {
   }
 }
 
-module.exports = { getProducts }
+async function getSingleProduct(req, res, id) {
+  try {
+    const product = await Product.findById(id)
+
+    res.writeHead(200, { 'Content-Type': 'application/json' })
+    res.end(JSON.stringify(product))
+  } catch (error) {
+    console.log(error)
+  }
+}
+
+module.exports = { getProducts, getSingleProduct }
--- server.js
+++ server.js
@@ -1,9 +1,18 @@
 const http = require('http')
-const { getProducts } = require('./controller/productsController')
+const {
+  getProducts,
+  getSingleProduct
+} = require('./controller/productsController')
 
 const server = http.createServer((req, res) => {
   if (req.url === '/api/products' && req.method === 'GET') {
     getProducts(req, res)
+  } else if (
+    req.url.match(/\/api\/products\/([0-9])+/) &&
+    req.method === 'GET'
+  ) {
+    const id = req.url.split('/')[3]
+    getSingleProduct(req, res, id)
   } else {
     res.writeHead(404, { 'Content-Type': 'application/json' })
     res.end(JSON.stringify({ message: 'Invalid route' }))
