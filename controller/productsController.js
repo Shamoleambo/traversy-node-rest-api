0cc33adb6e2e6dd1d6e32318e3d4b856432d6029
Tiago Gomes <tidgomes@gmail.com>
2023-01-05 16:41:09 -0300

    create model and controller

---
+++ controller/productsController.js
@@ -0,0 +1,13 @@
+const Product = require('../model/productModel')
+
+async function getProducts(req, res) {
+  try {
+    const products = await Product.findAll()
+    res.writeHead(200, { 'Content-Type': 'application/json' })
+    res.end(JSON.stringify(products))
+  } catch (error) {
+    console.log(error)
+  }
+}
+
+module.exports = { getProducts }
